045a57c98354ab583d7730fa5c7dd00cd7dea4f8
Daniel O <danielolvera2188@gmail.com>
2021-08-22 14:01:40 -0600

    added post route, corrected get route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -9,7 +9,7 @@ router.get('/api/notes', (req, res) => {
         if (err) throw err;
         console.log(JSON.parse(data));
 
-        res.send(db)
+        res.send(data)
     })
 })
 
@@ -20,13 +20,29 @@ router.post('/api/notes', (req, res) => {
         text: req.body.text
     }
     //  console.log(typeof db)
-    //1. use fs to read the db.json
+    // //1. use fs to read the db.json
     fs.readFile('./db/db.json', (err, data) => {
         if (err) throw err;
-    })
-    //2. create a variable and parse the data which got returned
-    //3. push the new Note to that parsed data
-    //4. use fs to write new content of that parsed data to db.json file
+
+        let newData = JSON.parse(data);
+
+        newData.push(newNote);
+        console.log(newData)
+
+        fs.writeFile('./db/db.json', JSON.stringify(newData), (err) => {
+            if (err) throw err;
+
+            res.send('successfully added');
+        })
+    });
+    // //2. create a variable and parse the data which got returned
+
+    // //3. push the new Note to that parsed data
+
+    // //4. use fs to write new content of that parsed data to db.json file
+    // req.body.id = notes.length.toString();
+
+
 })
 
 module.exports = router;
